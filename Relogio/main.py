8e8274b473b7b36abd5784610aa9baf6819448c9
Ange-lo06 <111506370+Ange-lo06@users.noreply.github.com>
2025-07-29 11:30:29 -0300

    Add files via upload

---
+++ Relogio/main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import time
+
+def atualizar_hora():
+    hora_atual = time.strftime('%H:%M:%S')
+    texto.config(text=hora_atual)
+    janela.after(1000, atualizar_hora)
+
+janela = tk.Tk()
+janela.title('Relogio Digital')
+janela.geometry('250x100')
+janela.configure(bg='black')
+
+texto = tk.Label(janela, font=('Courier', 40), bg='black', fg='lime')
+texto.pack(anchor='center')
+
+atualizar_hora()
+
+janela.mainloop()
